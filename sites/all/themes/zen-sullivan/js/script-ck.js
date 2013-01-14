00302870035a01cf57372f2073f987547b034d17
Marlene Williams <marlene@designbymarlene.com>
2013-01-13 22:31:40 -0800

    Adding new Zen theme and subtheme.

---
+++ sites/all/themes/zen-sullivan/js/script-ck.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - http://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function(e,t,n,r,i){t.behaviors.customToolbar={attach:function(t,r){e("body").hasClass("toolbar")&&e(n,t).resize(function(){var t=e("div#toolbar").height();e(".region-user").css("top",t+"px")})}}})(jQuery,Drupal,this,this.document);
---
+++ sites/all/themes/zen-sullivan/js/script.js
@@ -0,0 +1,27 @@
+/**
+ * @file
+ * A JavaScript file for the theme.
+ *
+ * In order for this JavaScript to be loaded on pages, see the instructions in
+ * the README.txt next to this file.
+ */
+
+// JavaScript should be made compatible with libraries other than jQuery by
+// wrapping it with an "anonymous closure". See:
+// - http://drupal.org/node/1446420
+// - http://www.adequatelygood.com/2010/3/JavaScript-Module-Pattern-In-Depth
+(function ($, Drupal, window, document, undefined) {
+
+  // Handle user toolbar when user is admin and have admin toolbar enabled.
+  Drupal.behaviors.customToolbar = {
+    attach: function(context, settings) {
+      if ($('body').hasClass('toolbar')) {
+        $(window, context).resize(function() {
+          var toolbarHeight = $('div#toolbar').height();
+          $('.region-user').css('top', toolbarHeight + 'px');
+        });
+      }
+    }
+  };
+
+})(jQuery, Drupal, this, this.document);
