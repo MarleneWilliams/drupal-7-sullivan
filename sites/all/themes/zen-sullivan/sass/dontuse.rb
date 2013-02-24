1d9f55c369094d5fbe774dec2ed6834ee1139f37
Marlene Williams <marlene@designbymarlene.com>
2013-02-24 14:43:05 -0800

    Adding newly compiled stylesheets.

---
+++ sites/all/themes/zen-sullivan/sass/dontuse.rb
@@ -0,0 +1,9 @@
+# Location of the theme's resources.http_path = "/"
+images_dir = "../images"
+css_dir         = "../css"
+sass_dir        = "."
+extensions_dir  = "sass-extensions"
+javascripts_dir = "js"
+output_style = :compressed
+relative_assets=true
+line_comments = false
