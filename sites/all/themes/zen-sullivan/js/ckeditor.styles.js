c4b26f47b8bcb464b0181c65bfc0faf9876adf2c
Marlene Williams <marlene@designbymarlene.com>
2013-03-07 23:43:29 -0800

    Adding in some image float styles.

--- sites/all/themes/zen-sullivan/js/ckeditor.styles.js
+++ sites/all/themes/zen-sullivan/js/ckeditor.styles.js
@@ -1,91 +1,87 @@
-﻿/**
- * Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.html or http://ckeditor.com/license
+/*
+Copyright (c) 2003-2012, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+/*
+ * This file is used/requested by the 'Styles' button.
+ * The 'Styles' button is not enabled by default in DrupalFull and DrupalFiltered toolbars.
  */
+if(typeof(CKEDITOR) !== 'undefined') {
+    CKEDITOR.addStylesSet( 'drupal',
+    [
+            /* Block Styles */
 
-// This file contains style definitions that can be used by CKEditor plugins.
-//
-// The most common use for it is the "stylescombo" plugin, which shows a combo
-// in the editor toolbar, containing all styles. Other plugins instead, like
-// the div plugin, use a subset of the styles on their feature.
-//
-// If you don't have plugins that depend on this file, you can simply ignore it.
-// Otherwise it is strongly recommended to customize this file to match your
-// website requirements and design properly.
+            // These styles are already available in the "Format" drop-down list, so they are
+            // not needed here by default. You may enable them to avoid placing the
+            // "Format" drop-down list in the toolbar, maintaining the same features.
+            /*
+            { name : 'Paragraph'		, element : 'p' },
+            { name : 'Heading 1'		, element : 'h1' },
+            { name : 'Heading 2'		, element : 'h2' },
+            { name : 'Heading 3'		, element : 'h3' },
+            { name : 'Heading 4'		, element : 'h4' },
+            { name : 'Heading 5'		, element : 'h5' },
+            { name : 'Heading 6'		, element : 'h6' },
+            { name : 'Preformatted Text', element : 'pre' },
+            { name : 'Address'			, element : 'address' },
+            */
 
-CKEDITOR.stylesSet.add( 'default', [
-	/* Block Styles */
+            /*{ name : 'Blue Title'		, element : 'h3', styles : { 'color' : 'Blue' } },
+            { name : 'Red Title'		, element : 'h3', styles : { 'color' : 'Red' } },*/
 
-	// These styles are already available in the "Format" combo ("format" plugin),
-	// so they are not needed here by default. You may enable them to avoid
-	// placing the "Format" combo in the toolbar, maintaining the same features.
-	/*
-	{ name: 'Paragraph',		element: 'p' },
-	{ name: 'Heading 1',		element: 'h1' },
-	{ name: 'Heading 2',		element: 'h2' },
-	{ name: 'Heading 3',		element: 'h3' },
-	{ name: 'Heading 4',		element: 'h4' },
-	{ name: 'Heading 5',		element: 'h5' },
-	{ name: 'Heading 6',		element: 'h6' },
-	{ name: 'Preformatted Text',element: 'pre' },
-	{ name: 'Address',			element: 'address' },
-	*/
+            /* Inline Styles */
 
-	{ name: 'Italic Title',		element: 'h2', styles: { 'font-style': 'italic' } },
-	{ name: 'Subtitle',			element: 'h3', styles: { 'color': '#aaa', 'font-style': 'italic' } },
-	{
-		name: 'Special Container',
-		element: 'div',
-		styles: {
-			padding: '5px 10px',
-			background: '#eee',
-			border: '1px solid #ccc'
-		}
-	},
+            // These are core styles available as toolbar buttons. You may opt enabling
+            // some of them in the "Styles" drop-down list, removing them from the toolbar.
+            /*
+            { name : 'Strong'			, element : 'strong', overrides : 'b' },
+            { name : 'Emphasis'			, element : 'em'	, overrides : 'i' },
+            { name : 'Underline'		, element : 'u' },
+            { name : 'Strikethrough'	, element : 'strike' },
+            { name : 'Subscript'		, element : 'sub' },
+            { name : 'Superscript'		, element : 'sup' },
+            */
 
-	/* Inline Styles */
+/*            { name : 'Marker: Yellow'	, element : 'span', styles : { 'background-color' : 'Yellow' } },
+            { name : 'Marker: Green'	, element : 'span', styles : { 'background-color' : 'Lime' } },
 
-	// These are core styles available as toolbar buttons. You may opt enabling
-	// some of them in the Styles combo, removing them from the toolbar.
-	// (This requires the "stylescombo" plugin)
-	/*
-	{ name: 'Strong',			element: 'strong', overrides: 'b' },
-	{ name: 'Emphasis',			element: 'em'	, overrides: 'i' },
-	{ name: 'Underline',		element: 'u' },
-	{ name: 'Strikethrough',	element: 'strike' },
-	{ name: 'Subscript',		element: 'sub' },
-	{ name: 'Superscript',		element: 'sup' },
-	*/
+            { name : 'Big'				, element : 'big' },
+            { name : 'Small'			, element : 'small' },
+            { name : 'Typewriter'		, element : 'tt' },
 
-	/* Object Styles */
+            { name : 'Computer Code'	, element : 'code' },
+            { name : 'Keyboard Phrase'	, element : 'kbd' },
+            { name : 'Sample Text'		, element : 'samp' },
+            { name : 'Variable'			, element : 'var' },
 
-	{
-		name: 'Styled image (left)',
-		element: 'img',
-		attributes: { 'class': 'left' }
-	},
+            { name : 'Deleted Text'		, element : 'del' },
+            { name : 'Inserted Text'	, element : 'ins' },
 
-	{
-		name: 'Styled image (right)',
-		element: 'img',
-		attributes: { 'class': 'right' }
-	},
+            { name : 'Cited Work'		, element : 'cite' },
+            { name : 'Inline Quotation'	, element : 'q' },
 
-	{
-		name: 'Compact table',
-		element: 'table',
-		attributes: {
-			cellpadding: '5',
-			cellspacing: '0',
-			border: '1',
-			bordercolor: '#ccc'
-		},
-		styles: {
-			'border-collapse': 'collapse'
-		}
-	},
+            { name : 'Language: RTL'	, element : 'span', attributes : { 'dir' : 'rtl' } },
+            { name : 'Language: LTR'	, element : 'span', attributes : { 'dir' : 'ltr' } }, */
 
-	{ name: 'Borderless Table',		element: 'table',	styles: { 'border-style': 'hidden', 'background-color': '#E6E6FA' } },
-	{ name: 'Square Bulleted List',	element: 'ul',		styles: { 'list-style-type': 'square' } }
-]);
+            /* Object Styles */
 
+            {
+                    name : 'Image on Left',
+                    element : 'img',
+                    attributes :
+                    {
+                            'class' : 'float-left'
+                    }
+            },
+
+            {
+                    name : 'Image on Right',
+                    element : 'img',
+                    attributes :
+                    {
+                            'class' : 'float-right'
+                    }
+            }
+    ]);
+}
